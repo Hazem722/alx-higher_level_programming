d0f663a2f6166ba249636a2f3079e10972232bee
Hazem722 <hazemba684@gmail.com>
2022-09-22 06:08:57 -0700

    2. How to make a script dynamic! git add . added case 0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,9 @@
 if __name__ == '__main__':
     import sys
     arg_num = len(sys.argv) - 1
-    if arg_num == 1:
+    if arg_num == 0:
+        print("{} arguments.".format(arg_num))
+    elif arg_num == 1:
         print("{} argument:".format(arg_num))
     else:
         print("{} arguments:".format(arg_num))

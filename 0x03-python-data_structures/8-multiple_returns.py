0d00fb0ff8ed8cffff15b250572ba2cc295bf8e2
Hazem722 <hazemba684@gmail.com>
2022-09-26 08:26:33 -0700

    8. More returns!

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence):
+        first_caracter = sentence[0]
+    first_caracter = None
+    return (len(sentence), first_caracter)

bd5608c1f5e2b23ede5812dd9cab46bcdc986110
Hazem722 <hazemba684@gmail.com>
2022-10-10 17:26:31 -0700

    do not use bare except fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for i in range(0, x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except TypeError:
-            pass
-        finally:
-            print()
-            return count
+    try:
+        for i in my_list:
+            if count < x:
+                print("{:d}".format(my_list[count]), end="")
+                count += 1
+        print()
+    except TypeError:
+        pass
+    finally:
+        return count

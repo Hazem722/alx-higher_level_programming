3d1edafdf5ec35ddff25994ce4c5c1112a18a5b2
Hazem722 <hazemba684@gmail.com>
2023-01-17 13:49:14 +0100

    0. What's my status? #0 fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,11 @@
 """Fetches https://intranet.hbtn.io/status"""
 
 
-if __name__ == "__main__":
-    import urllib.request
+import urllib.request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
 
-    print('Body response:\n\t- type: {}'.format(type(html)))
-    print('\t- content: {}'.format(html))
-    print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
+print('Body response:\n\t- type: {}'.format(type(html)))
+print('\t- content: {}'.format(html))
+print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))

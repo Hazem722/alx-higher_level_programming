336914945af3a1883d9f1e38e4a9865167387553
Hazem722 <hazemba684@gmail.com>
2023-01-17 14:17:45 +0100

    4. What's my status? #1 fixed

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status"""
-
-
 if __name__ == "__main__":
     import requests
-
     r = requests.get('https://intranet.hbtn.io/status')
+    response = r.text
 
     print('Body response:')
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
+    print('\t- type: {}'.format(type(response)))
+    print('\t- content: {}'.format(response))
